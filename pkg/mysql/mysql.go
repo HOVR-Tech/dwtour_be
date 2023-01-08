03f1a030526b99352dcf2ae2a965b5e173849f2d
JavaRich <109426430+HOVR-Tech@users.noreply.github.com>
2023-01-09 02:03:12 +0700

    Update mysql.go

--- pkg/mysql/mysql.go
+++ pkg/mysql/mysql.go
@@ -11,7 +11,7 @@ var DB *gorm.DB
 
 func DatabaseInit() {
 	var err error
-	dsn := "root:@tcp(127.0.0.1:3306)/dewe_tour?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "root:CjRbLKz7azXRw9TsWEEp@tcp(containers-us-west-131.railway.app:5705)/railway?charset=utf8mb4&parseTime=True&loc=Local"
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
